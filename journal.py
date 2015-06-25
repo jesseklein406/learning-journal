3f0b2f306f11df230873a540e8b456a4ea8f5c74
Jesse Klein <jesse@Jesses-MacBook-Air.local>
2015-06-24 20:36:55 -0700

    Add transaction manager
    Add ZopeTransactionExtension

--- journal.py
+++ journal.py
@@ -11,6 +11,7 @@ from waitress import serve
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+from zope.sqlalchemy import ZopeTransactionExtension
 
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
