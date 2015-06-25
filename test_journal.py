6edfafb7e89cc0e4d2589bc418ae8fa1a2ec77d9
Jesse Klein <jesse@Jesses-MacBook-Air.local>
2015-06-24 20:19:18 -0700

    Fix DATABASE_URL typo
    Fix info in DATABASE_URL definition

---
+++ test_journal.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import os
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
+
+TEST_DATABASE_URL = os.environ.get(
+    'DATABASE_URL',
+    'postgresql://jesse:Jjk5646!@localhost:5432/test-learning-journal'
+)
+os.environ['DATABASE_URL'] = TEST_DATABASE_URL
+os.environ['TESTING'] = "True"
+
+import journal
+
